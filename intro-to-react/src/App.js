557eb8b556a860fc749b0c8caf513499bc0d02e9
Xola Manciya <xola.manciya@yoco.com>
2022-04-25 20:45:37 +0200

    Created the footer

--- intro-to-react/src/App.js
+++ intro-to-react/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
       <Header greeting="Hello World"/>
+      <Footer companyName="Dunder Mifflin"/>
     </div>
   );
 }
